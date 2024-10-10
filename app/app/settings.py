3ab775d29485d40cb8543a953dd943351e912164
raul crespo <raulxcrespo@gmail.com>
2024-10-10 13:32:27 -0300

    feat: add paths to accounts app and authentication

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+LOGIN_REDIRECT_URL = '/api/v1/accounts/users'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
